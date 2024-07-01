97f93463b3e4e07cd199569ffaafbd888a3c1c7d
Ian Stainer <ian-stainer@users.noreply.github.com>
2024-07-01 22:56:04 +0100

    fset-3244 fixed PR warnings

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -18,9 +18,4 @@ import sbt.Tests.{Group, SubProcess}
 import sbt._
 
 private object TestPhases {
-
-//  def oneForkedJvmPerTest(tests: Seq[TestDefinition]) =
-//    tests map {
-//      test => new Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name))))
-//    }
 }
